c69db5e295df6bd7330df23954ec4c64b59fcb3a
master <master@126.com>
2011-11-30 16:35:48 +0800

    modify test.sh new add a file bash_arc.png
    Signed-off-by: master <master@126.com>

--- bash/test.sh
+++ bash/test.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+#####################################################################
+#source a bash script
+#source ./time.sh or ../time.sh
+
 #echo "JIN" >>jin.v
 #jin='date'
 #echo $jin>>jin.v
@@ -371,9 +375,12 @@ test_name  #调用定义的子函数
 JIN_test () {
 	echo $1
 	echo $2 
+	JIN=$(echo $2  | sed  's/XIE/XIE___XXX/')
+	#var=$(echo $var | sed 's/./\\&/g')
+	echo "$JIN Google"
 }
 
-JIN_test JIN XIE #JIN-->$1 XIE-->$2
+JIN_test JIN XIE_SET #JIN-->$1 XIE-->$2 #调用定义的函数
 ###################################################################################
 until [ -z "$1" ]  # Until all parameters used up
 do
@@ -387,5 +394,36 @@ do
 done
 
 	echo $? #返回值0代表成功
+
+###################################################################################
+#运算符	描述	示例
+#文件比较运算符
+# -e filename	如果 filename存在，则为真	[ -e /var/log/syslog ]
+# -d filename	如果 filename为目录，则为真	[ -d /tmp/mydir ]
+# -f filename	如果 filename为常规文件，则为真	[ -f /usr/bin/grep ]
+# -L filename	如果 filename为符号链接，则为真	[ -L /usr/bin/grep ]
+# -r filename	如果 filename可读，则为真	[ -r /var/log/syslog ]
+# -w filename	如果 filename可写，则为真	[ -w /var/mytmp.txt ]
+# -x filename	如果 filename可执行，则为真	[ -L /usr/bin/grep ]
+# filename1 -nt filename2	如果 filename1比 filename2新，则为真	[ /tmp/install/etc/services -nt /etc/services ]
+# filename1 -ot filename2	如果 filename1比 filename2旧，则为真	[ /boot/bzImage -ot arch/i386/boot/bzImage ]
+
+#字符串比较运算符 （请注意引号的使用，这是防止空格扰乱代码的好方法）
+# -z string	如果 string长度为零，则为真	[ -z "$myvar" ]
+# -n string	如果 string长度非零，则为真	[ -n "$myvar" ]
+# string1 = string2	如果 string1与 string2相同，则为真	[ "$myvar" = "one two three" ]
+# string1 != string2	如果 string1与 string2不同，则为真	[ "$myvar" != "one two three" ]
+
+#算术比较运算符
+#num1 -eq num2	等于	    [ 3 -eq $mynum ]
+#num1 -ne num2	不等于	    [ 3 -ne $mynum ]
+#num1 -lt num2	小于	    [ 3 -lt $mynum ]
+#num1 -le num2	小于或等于	[ 3 -le $mynum ]
+#num1 -gt num2	大于	    [ 3 -gt $mynum ]
+#num1 -ge num2	大于或等于	[ 3 -ge $mynum ]
+
+###################################################################################
+#正则表达式
+
 
 exit
