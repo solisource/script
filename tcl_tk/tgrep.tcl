fef61c17fe965badb5ad0afe2dd528138e072275
master <master@126.com>
2012-02-29 18:05:13 +0800

    REALSIL-AF75F,ds_jin,d:/master/script,12/02/29-18/05/04

---
+++ tcl_tk/tgrep.tcl
@@ -0,0 +1,14 @@
+
+proc tgrep {pattern filename} { 
+	set f [open $filename r] 
+	while { [gets $f line ] } { 
+		if {[regexp $pattern $line]} { 
+		puts stdout $line 
+		} 
+	} 
+	close $f 
+}
+
+tgrep "REF" "file_name"
+
+
